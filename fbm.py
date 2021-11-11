1b2814321248373fa30a00de1f7c816cc6d12135
Jannis Baum <jannis.baum@gmail.com>
2021-11-11 21:03:54 +0100

    open fbms in list by regex match

--- fbm.py
+++ fbm.py
@@ -1,5 +1,6 @@
 import os
 import argparse
+import re
 
 class FBMNode:
     def __init__(self, depth, directory, fbms):
@@ -15,25 +16,46 @@ class FBMList:
     DIR_NAME = '.fbms'
 
     def __init__(self):
-        self.fbms = []
+        self.nodes = []
         self.build_list(os.getcwd(), 0)
 
     def build_list(self, path, depth):
         if path == '/Users': return
         fbms_dir = os.path.join(path, FBMList.DIR_NAME)
         if os.path.isdir(fbms_dir):
-            self.fbms.append(FBMNode(depth, path, os.listdir(fbms_dir)))
+            self.nodes.append(FBMNode(depth, path, os.listdir(fbms_dir)))
         self.build_list(os.path.dirname(path), depth + 1)
 
     def print(self):
-        print('\n\n'.join([fbm.string() for fbm in self.fbms]))
+        print('\n\n'.join([fbm.string() for fbm in self.nodes]))
+
+    def find_match(self, pattern):
+        nodes = self.nodes
+        if '/' in pattern:
+            pattern = pattern.split('/')
+            pattern_dir = pattern.pop(0)
+            pattern = ''.join(pattern)
+            if pattern_dir != '':
+                nodes = [fbm for fbm in nodes if re.match(pattern_dir, os.path.basename(fbm.directory))]
+        if pattern != '':
+            for node in nodes:
+                for fbm in node.fbms:
+                    if re.match(pattern, fbm):
+                        os.system(f'{node.directory}/{FBMList.DIR_NAME}/{fbm}')
+                        return
+        print(f'no match found :(')
+
+def main(args):
+    fbms = FBMList()
+    if args.name:
+        fbms.find_match(args.name)
+        return
+    if args.list:
+        fbms.print()
+        return
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('name', nargs='?', default=None)
     parser.add_argument('-l', '--list', action='store_true', dest='list', help='list fbms in directory tree')
-    args = parser.parse_args()
-
-    fbms = FBMList()
-
-    if args.list:
-        fbms.print()
+    main(parser.parse_args())
