624d7a7b8c25f7fddbfa67bffccf0624178dfcf9
Jannis Baum <jannis.baum@gmail.com>
2021-11-12 13:20:46 +0100

    infrastructure to create and use yaml based fbms

--- fbm.py
+++ fbm.py
@@ -1,6 +1,7 @@
 import os
 import argparse
 import re
+import yaml
 
 class FBMNode:
     def __init__(self, depth, directory, fbms):
@@ -14,6 +15,8 @@ class FBMNode:
 
 class FBMList:
     DIR_NAME = '.fbms'
+    ID_URL = 'url'
+    ID_COPYTEXT = 'copy-text'
 
     def __init__(self):
         self.nodes = []
@@ -29,7 +32,7 @@ class FBMList:
     def print(self):
         print('\n\n'.join([fbm.string() for fbm in self.nodes]))
 
-    def find_match(self, pattern):
+    def open_match(self, pattern):
         nodes = self.nodes
         if '/' in pattern:
             pattern = pattern.split('/')
@@ -41,21 +44,43 @@ class FBMList:
             for node in nodes:
                 for fbm in node.fbms:
                     if re.match(pattern, fbm):
-                        os.system(f'{node.directory}/{FBMList.DIR_NAME}/{fbm}')
+                        with open(os.path.join(node.directory, FBMList.DIR_NAME, fbm)) as data_yaml:
+                            data = yaml.safe_load(data_yaml.read())
+                            if data[FBMList.ID_COPYTEXT]:
+                                os.system(f'printf \%s "{data[FBMList.ID_COPYTEXT]}" | pbcopy')
+                            os.system('open ' + data[FBMList.ID_URL])
                         return
         print(f'no match found :(')
+
+def new_fbm(path, name, url, copy_text):
+    path = os.path.join(path, FBMList.DIR_NAME)
+    if not os.path.isdir(path):
+        os.mkdir(path)
+    with open(os.path.join(path, name), 'w') as fbm:
+        fbm.write(yaml.dump({
+            FBMList.ID_URL: url,
+            FBMList.ID_COPYTEXT: copy_text
+        }))
 
 def main(args):
     fbms = FBMList()
     if args.name:
-        fbms.find_match(args.name)
+        fbms.open_match(args.name)
         return
     if args.list:
         fbms.print()
         return
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('name', nargs='?', default=None)
-    parser.add_argument('-l', '--list', action='store_true', dest='list', help='list fbms in directory tree')
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
+    mode = parser.add_mutually_exclusive_group()
+    mode.add_argument('name', nargs='?', default=None)
+    mode.add_argument('-l', '--list', action='store_true', dest='list', help='list fbms in directory tree')
+    mode.add_argument('-n', '--new', dest='new', nargs='+', metavar=('name url', 'copy-text'), type=str,
+                      help=f'create new fbm in closest {FBMList.DIR_NAME} directory')
+    mode.add_argument('-ni', '--new-in', dest='new_in', nargs='+', metavar=('directory name url', 'copy-text'), type=str,
+                      help=f'create new fbm in matching {FBMList.DIR_NAME} directory')
+    mode.add_argument('-nh', '--new-here', dest='new_here', nargs='+', metavar=('name url', 'copy-text'), type=str,
+                      help=f'create new fbm in current directory')
+
     main(parser.parse_args())
